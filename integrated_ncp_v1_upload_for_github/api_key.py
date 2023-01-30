1280fff499838e10579dfc639a772d845e08338f
kjhooni <73940434+kjhooni@users.noreply.github.com>
2023-01-30 17:37:13 +0900

    Add files via upload

---
+++ integrated_ncp_v1_upload_for_github/api_key.py
@@ -0,0 +1,17 @@
+###운영1~3팀 ACCESS_KEY
+ncloud_accesskey = {
+    1: "example",
+    2: "example",
+    3: "example",
+}
+
+###운영1~3팀 SECRET_KEY
+ncloud_secretkey = {
+    1: "example",
+    2: "example",
+    3: "example",
+}
+
+###운영2팀 api키
+ncloud_accesskey_se2 = "example"
+ncloud_secretkey_se2 = "example"
---
+++ integrated_ncp_v1_upload_for_github/recipients.py
@@ -0,0 +1,5 @@
+list = {
+    1: "honggilldong@aa.com",
+    2: "honggilldong2@aa.com",
+    3: "honggilldong3@aa.com",
+}
