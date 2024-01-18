481f53050314acb9893cef897676ddc7fa3f9081
kjhooni <73940434+kjhooni@users.noreply.github.com>
2024-01-18 13:13:21 +0900

    Add files via upload

---
+++ integrated_ncp_v1.1_upload_for_github/api_key.py
@@ -0,0 +1,17 @@
+### ACCESS_KEY
+ncloud_accesskey = {
+    1: "example",
+    2: "example",
+    3: "example",
+}
+
+### SECRET_KEY
+ncloud_secretkey = {
+    1: "example",
+    2: "example",
+    3: "example",
+}
+
+### VIP3팀 api키
+ncloud_accesskey_se2 = "example"
+ncloud_secretkey_se2 = "example"
---
+++ integrated_ncp_v1.1_upload_for_github/recipients.py
@@ -0,0 +1,5 @@
+list = {
+    1: "example@didim365.com",
+    2: "example@didim365.com",
+    3: "example@didim365.com",
+}
